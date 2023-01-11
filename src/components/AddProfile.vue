11345a037520217a6b40b2b03446cb1983358b5f
LY65822 <LY65822@DERIW46ZLPC0368>
2023-01-11 22:06:07 +0530

    AddProfile

--- src/components/AddProfile.vue
+++ src/components/AddProfile.vue
@@ -5,11 +5,11 @@
         <q-card class="q-pa-lg cardsize">
           <q-form class="q-gutter-md vertical-center"
             ><br />
-              <center>
+            <center>
               <q-input
                 filled
                 type="text"
-                v-model="Id"
+                v-model="Profiles.id"
                 label="Enter Person ID *"
                 lazy-rules
                 :rules="[
@@ -21,7 +21,7 @@
               <q-input
                 filled
                 type="text"
-                v-model="First_name"
+                v-model="Profiles.Frist_Name"
                 label="Enter Person First Name *"
                 lazy-rules
                 :rules="[
@@ -33,7 +33,7 @@
               <q-input
                 filled
                 type="text"
-                v-model="Last_Name"
+                v-model="Profiles.Last_Name"
                 label="Enter Person Last Name *"
                 lazy-rules
                 :rules="[
@@ -45,7 +45,7 @@
               <q-input
                 filled
                 type="text"
-                v-model="City"
+                v-model="Profiles.City"
                 label="Enter Person City *"
                 lazy-rules
                 :rules="[
@@ -57,7 +57,7 @@
               <q-input
                 filled
                 type="text"
-                v-model="PhoneNumber"
+                v-model="Profiles.PhoneNumber"
                 label="Enter Person Phone Number *"
                 lazy-rules
                 :rules="[
@@ -70,7 +70,7 @@
               <q-input
                 filled
                 type="text"
-                v-model="department"
+                v-model="Profiles.department"
                 label="Enter Person Department *"
                 lazy-rules
                 :rules="[
@@ -81,13 +81,13 @@
 
               <div>
                 <center>
-                <q-btn
-                  label="Submit"
-                  type="button"
-                  v-on:click="AddProfile"
-                  color="green"
-                ></q-btn>
-              </center>
+                  <q-btn
+                    label="Submit"
+                    type="button"
+                    v-on:click="AddProfile"
+                    color="green"
+                  ></q-btn>
+                </center>
               </div>
             </center>
           </q-form>
@@ -97,63 +97,58 @@
   </q-page>
 </template>
 
- <script>
-//import axios from "axios";
+ <script lang="ts">
+import axios from "axios";
 import { ref } from "vue";
+import HomeViewVue from "@/views/HomeView.vue";
+import router from "@/router";
 export default {
   name: "AddProfile",
   setup() {
-    // const Id = ref<string>("");
-    // const Frist_Name = ref<string>("");
-    // const Last_Name = ref<string>("");
-    // const City = ref<string>("");
-    // const PhoneNumber = ref<string>("");
-    // const department = ref<string>("");
-    const Id = ref("");
-    const Frist_Name = ref("");
-    const Last_Name = ref("");
-    const City = ref("");
-    const PhoneNumber = ref("");
-    const department = ref("");
-    // async function AddProfile() {
-    //   console.warn(this.Profiles);
-    //   const data = await axios.post("http://localhost:3000/Profiles", {
-    //     Id: this.Profiles.Id,
-    //     Frist_Name: this.Profiles.Frist_Name,
-    //     Last_Name: this.Profiles.Last_Name,
-    //     City: this.Profiles.City,
-    //     PhoneNumber: this.Profiles.PhoneNumber,
-    //     department: this.Profiles.department,
-    //   });
-    //   if (data.status == 201) {
-    //     this.$router.push({ name: "/" });
-    //   } else {
-    //     console.warn("data", data);
-    //     alert("AddProfile  Successfully");
-    //   }
-    // }
+    const Profiles = ref<any>([
+      {
+        id: ref<string>(""),
+        Frist_Name: ref<string>(""),
+        Last_Name: ref<string>(""),
+        City: ref<string>(""),
+        PhoneNumber: ref<string>(""),
+        department: ref<string>(""),
+      },
+    ]);
+    async function AddProfile() {
+      const data = await axios.post("http://localhost:3000/Profiles", {
+        id: Profiles.value.id,
+        Frist_Name: Profiles.value.Frist_Name,
+        Last_Name: Profiles.value.Last_Name,
+        City: Profiles.value.City,
+        PhoneNumber: Profiles.value.PhoneNumber,
+        department: Profiles.value.department,
+      });
+      console.log(data);
+      if (data.status === 201) {
+        router.push({ name: "HomeView" });
+      } else {
+        alert("AddProfile Successfully");
+      }
+    }
     return {
-      Id,
-      PhoneNumber,
-      Last_Name,
-      Frist_Name,
-      City,
-      department
+      AddProfile,
+      Profiles,
     };
   },
 };
 </script>
 <style scoped>
-.inputbox{
-  padding:5px;
-  width:350px;
+.inputbox {
+  padding: 5px;
+  width: 350px;
 }
-.totalpage{
+.totalpage {
   padding-top: 100px;
-  position: fixed;
-  padding-left:200px;
+  position: center;
+  padding-left: 200px;
 }
-.cardsize{
-  width:1000px;
+.cardsize {
+  width: 1000px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,58 @@
 <template>
-  <div class="homeviewpage">
-    <search-component class="searchbar" />
-    <q-btn color="primary" class="addprofile" router-link to = "/AddProfile" label="Add Prifile" />
-    <profile-card  class="profilecard" />
-  </div>
+  <q-page class="column items-center justify-center totalpage">
+    <div class="q-pa-md bg-image">
+      <q-layout class="vertical-center">
+        <q-card class="q-pa-lg cardsize">
+          <div class="homeviewpage">
+            <search-component class="searchbar" />
+            <q-btn
+              color="primary"
+              class="addprofile"
+              router-link
+              to="/AddProfile"
+              label="Add Prifile"
+            />
+          </div>
+          <div class="profilecard">
+            <profile-card class="image" />
+          </div>
+        </q-card>
+      </q-layout>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
-import SearchComponent from '@/components/SearchComponent.vue';
+import SearchComponent from "@/components/SearchComponent.vue";
 import ProfileCard from "../components/ProfileCard.vue";
-
+import axios from "axios";
 export default {
   name: "HomeView",
-  components: {ProfileCard,SearchComponent},
+  components: { ProfileCard, SearchComponent },
   setup() {
-    return {
-    SearchComponent
-      
-    };
+    return {};
   },
 };
 </script>
 <style scoped>
-.profilecard{
-  padding-top : 100px;
-  padding-left: 40px;
+.profilecard {
+  padding: 15px;
+  background-image: url("@/assets/images.jfif");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  border: solid black 2px;
 }
-.addprofile{
-  position:absolute;
-  right:50px;
-  top:100px;
+.cardsize {
+  width: 1300px;
 }
-.searchbar{
-  padding-top : 100px;
-  padding-left : 40px;
+.addprofile {
+  position: absolute;
+  right: 40px;
+  top: 40px;
+}
+.totalpage {
+  padding-top: 60px;
 }
 </style>
 
